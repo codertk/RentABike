6046106327f248d9de9f3fc5696a7ee1957fd263
codertk <thilakmsc@gmail.com>
2018-07-04 08:52:25 +0530

    First Push

---
+++ RentABike/Controllers/BikesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RentABike.Models;
+
+namespace RentABike.Controllers
+{
+    public class BikesController : Controller
+    {
+        //
+        // GET: /Bikes/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Bikes/Random
+        public ActionResult Random()
+        {
+            var bike = new Bike() {Name = "ThunderBird 350"};
+            //return View(bike);
+
+            return Content("Hello World");
+
+
+        }
+	}
+}
---
+++ RentABike/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RentABike.Startup))]
+namespace RentABike
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
