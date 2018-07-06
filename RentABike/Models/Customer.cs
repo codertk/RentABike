fa0e5877cd0afa84d1983b30953f89528e2b49b6
codertk <thilakmsc@gmail.com>
2018-07-06 21:30:49 +0530

    Section 3 - Exercise 2 - DateOfBirth

--- RentABike/Models/Customer.cs
+++ RentABike/Models/Customer.cs
@@ -12,6 +12,8 @@ namespace RentABike.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
         public bool IsSubscibedToNewsLetter { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
